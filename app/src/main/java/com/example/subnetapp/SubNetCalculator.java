57cbd6d983701bdb5e15370b330a859648d069d6
Janda95 (Mars) <j.rettinghouse@gmail.com>
2019-07-21 18:06:26 -0700

    updated subnet class

--- app/src/main/java/com/example/subnetapp/SubNetCalculator.java
+++ app/src/main/java/com/example/subnetapp/SubNetCalculator.java
@@ -1,4 +1,13 @@
 package com.example.subnetapp;
 
 public class SubNetCalculator {
+
+  private int rootSubnetInt = 0;
+  private String rootSubnetString = "NULL";
+
+  public SubNetCalculator(){
+
+  }
+
+
 }
