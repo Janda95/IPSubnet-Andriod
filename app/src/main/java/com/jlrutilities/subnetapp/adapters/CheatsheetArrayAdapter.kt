348c2476f57918cf0475386ffe52e685118a703a
Jarek Rettinghouse <j.rettinghouse@gmail.com>
2025-02-04 12:39:49 -0800

    Adapters Kotlin migration

--- app/src/main/java/com/jlrutilities/subnetapp/adapters/CheatsheetArrayAdapter.kt
+++ app/src/main/java/com/jlrutilities/subnetapp/adapters/CheatsheetArrayAdapter.kt
@@ -1,44 +1,52 @@
-package com.jlrutilities.subnetapp.adapters;
+package com.jlrutilities.subnetapp.adapters
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.jlrutilities.subnetapp.R
 
-import com.jlrutilities.subnetapp.R;
 
 //** Allows subnet context to be passed and reference view information. */
-public class CheatsheetArrayAdapter extends ArrayAdapter<String> {
-  private final Context context;
-  private final String[] bitsArr;
-  private final String[] netmaskArr;
-  private final String[] hostsArr;
+class CheatsheetArrayAdapter(
+        context: Context,
+        values: Array<String>,
+        netmaskArr: Array<String>,
+        hostsArr: Array<String>
+): ArrayAdapter<String>(context, -1, values){
 
-  //** Constructor for passing and referencing current subnet tree information. */
-  public CheatsheetArrayAdapter(Context context, String[] values, String[] netmaskArr, String[] hostsArr) {
-    super(context, -1, values);
+  private val context: Context
+  private val bitsArr: Array<String>
+  private val netmaskArr: Array<String>
+  private val hostsArr: Array<String>
+
+  init {
     this.context = context;
     this.bitsArr = values;
     this.netmaskArr = netmaskArr;
     this.hostsArr = hostsArr;
   }
 
+
   //** Sets subnet information in view. */
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    LayoutInflater inflater = (LayoutInflater) context
-        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    View rowView = inflater.inflate(R.layout.cheatsheet_list_item, parent, false);
+  override fun getView(
+          position: Int,
+          convertView: View?,
+          parent: ViewGroup
+  ): View {
+    val inflater: LayoutInflater = LayoutInflater.from(context)
+    // TODO: ViewHolder Pattern
+    val rowView: View = inflater.inflate(R.layout.cheatsheet_list_item, parent, false);
 
-    TextView bitsTv = rowView.findViewById(R.id.bitsItemTv);
-    TextView netmaskTv = rowView.findViewById(R.id.netmaskItemTv);
-    TextView hostsTv = rowView.findViewById(R.id.hostsItemTv);
+    val bitsTv: TextView = rowView.findViewById(R.id.bitsItemTv);
+    val netmaskTv: TextView = rowView.findViewById(R.id.netmaskItemTv);
+    val hostsTv: TextView = rowView.findViewById(R.id.hostsItemTv);
 
-    bitsTv.setText(bitsArr[position]);
-    netmaskTv.setText(netmaskArr[position]);
-    hostsTv.setText(hostsArr[position]);
+    bitsTv.text = bitsArr[position]
+    netmaskTv.text = netmaskArr[position]
+    hostsTv.text = hostsArr[position]
 
     return rowView;
   }
--- app/src/main/java/com/jlrutilities/subnetapp/adapters/IpArrayAdapter.kt
+++ app/src/main/java/com/jlrutilities/subnetapp/adapters/IpArrayAdapter.kt
@@ -1,47 +1,57 @@
 package com.jlrutilities.subnetapp.adapters;
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import com.jlrutilities.subnetapp.R
 
-import com.jlrutilities.subnetapp.R;
 
 //** Allows Ip information to be passed and reference view information. */
-public class IpArrayAdapter extends ArrayAdapter<String> {
-  private final Context context;
-  private final String[] values;
-  private final int[] cidrArr;
-  private final int[] hosts;
+class IpArrayAdapter(
+        context: Context,
+        values: Array<String>,
+        cidr: IntArray,
+        hosts: IntArray
+): ArrayAdapter<String>(context, -1, values){
 
-  //** Constructor for passing and referencing current IPv4 network information. */
-  public IpArrayAdapter(Context context, String[] values, int[] cidr, int[] hosts) {
-    super(context, -1, values);
+  private val context: Context
+  private val values: Array<String>
+  private val cidrArr: IntArray
+  private val hosts: IntArray
+
+  init {
     this.context = context;
     this.values = values;
     this.cidrArr = cidr;
     this.hosts = hosts;
   }
 
-  //** Sets IPv4 network information in view. */
-  @Override
-  public View getView(int position, View convertView, ViewGroup parent) {
-    LayoutInflater inflater = (LayoutInflater) context
-        .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-    View rowView = inflater.inflate(R.layout.ip_list_item, parent, false);
 
-    TextView ipTv = rowView.findViewById(R.id.tvIpItem);
-    TextView cidrTv = rowView.findViewById(R.id.cidrIpItem);
-    TextView hostsTv = rowView.findViewById(R.id.hostsTv);
+  override fun getView(
+          position: Int,
+          convertView: View?,
+          parent: ViewGroup
+  ): View {
+    // TODO: check layoutinflator is correct before update to store
+    val inflater: LayoutInflater = LayoutInflater.from(context)
+    // TODO: ViewHolderPattern
+    val rowView: View = inflater.inflate(R.layout.ip_list_item, parent, false)
 
-    String cidrStr = Integer.toString(cidrArr[position]);
-    String hostsStr = String.format("Usable Hosts: %,d", hosts[position]);
-    ipTv.setText(values[position]);
-    cidrTv.setText("/" + cidrStr);
-    hostsTv.setText(hostsStr);
+    val ipTv: TextView = rowView.findViewById(R.id.tvIpItem)
+    val cidrTv: TextView = rowView.findViewById(R.id.cidrIpItem)
+    val hostsTv: TextView = rowView.findViewById(R.id.hostsTv)
 
-    return rowView;
+    val cidrStr: String = "/" + cidrArr[position].toString()
+    val hostPosition: Int = hosts[position]
+    val hostsStr = "Usable Hosts: $hostPosition"
+
+    ipTv.text = values[position]
+    cidrTv.text = cidrStr
+    hostsTv.text = hostsStr
+
+    return rowView
   }
 }
